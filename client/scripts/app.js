e83eb0d5f0e872e3ec0a16fbfa44443a70523209
scottbarclay <scottdbarclay@gmail.com>
2018-01-29 16:05:36 -0800

    Add finished Siskel solution

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,8 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    return this.set('like', !this.get('like'));
+    
   }
 
 });
@@ -15,15 +16,15 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', this.sort);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;    
+    this.sort();
   }
-
 });
 
 var AppView = Backbone.View.extend({
@@ -58,7 +59,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.on('change', this.render());
   },
 
   events: {
@@ -66,7 +67,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +80,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
